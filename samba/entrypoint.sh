958f794b13f9aae7fd9c22eb3e28c35cd31cc457
sfobosde <grishavasilev62@gmail.com>
2026-07-14 22:52:41 +0300

    Прописал конфиги самбы

---
+++ samba/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+REALM="${SAMBA_REALM}"
+DOMAIN="${SAMBA_DOMAIN}"
+HOSTNAME="${SAMBA_HOSTNAME}"
+PASSWORD="${SAMBA_ADMIN_PASSWORD}"
+DNS_BACKEND="${SAMBA_DNS_BACKEND:-SAMBA_INTERNAL}"
+
+if [ ! -f /var/lib/samba/private/secrets.ldb ]; then
+    rm -f /etc/samba/smb.conf
+
+    samba-tool domain provision \
+        --realm="$REALM" \
+        --domain="$DOMAIN" \
+        --host-name="$HOSTNAME" \
+        --server-role=dc \
+        --dns-backend="$DNS_BACKEND" \
+        --adminpass="$PASSWORD"
+fi
+
+exec samba --foreground
